8ad74bd926940ae23a23e1f5c6d0405a001dee7c
Sam Lavigne <splavigne@gmail.com>
2021-06-22 18:47:48 -0400

    updates

--- search.py
+++ search.py
@@ -14,20 +14,27 @@ from nltk.tokenize import sent_tokenize
 
 
 def main(text, query, max_results=100, memoize=True):
+
+    # sentence tokenize our text
     corpus = sent_tokenize(text)
     corpus = [s.strip() for s in corpus]
     corpus = [s for s in corpus if s != "" and s != '"']
 
+    # load up the embedder
+    # for a full list of pretrained models, see: https://www.sbert.net/docs/pretrained_models.html
     # embedder = SentenceTransformer("paraphrase-distilroberta-base-v1")
     embedder = SentenceTransformer("paraphrase-MiniLM-L6-v2")
 
+    # hash the text for memoization
     hash_object = hashlib.md5(text.encode())
     picklename = f"embeddings_{hash_object.hexdigest()}.pkl"
 
+    # if we've already processed the text, load it
     if os.path.exists(picklename):
         with open(picklename, "rb") as filein:
             corpus_embeddings = pickle.load(filein)
     else:
+        # otherwise make the embeddings
         corpus_embeddings = embedder.encode(
             corpus, convert_to_tensor=True, show_progress_bar=True
         )
@@ -37,19 +44,22 @@ def main(text, query, max_results=100, memoize=True):
                     corpus_embeddings, fileout, protocol=pickle.HIGHEST_PROTOCOL
                 )
 
-    # Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
+    # how many results should we return?
     top_k = min(max_results, len(corpus))
+
+    # generate embeddings for the query
     query_embedding = embedder.encode(
         query, convert_to_tensor=True, show_progress_bar=False
     )
 
-    # We use cosine-similarity and torch.topk to find the highest 5 scores
+    # Find the the sentences most similar to our query based cosine similarity
+    # note that top_results[0] contains the scores, and top_results[1] indices
     cos_scores = util.pytorch_cos_sim(query_embedding, corpus_embeddings)[0]
     top_results = torch.topk(cos_scores, k=top_k)
+    indices = top_results[1]
+    results = [corpus[i] for i in indices]
 
-    for score, idx in zip(top_results[0], top_results[1]):
-        # print(corpus[idx], "(Score: {:.4f})".format(score))
-        print(corpus[idx])
+    return results
 
 
 if __name__ == "__main__":
@@ -63,4 +73,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     text = args.infile.read()
 
-    main(text, args.query, args.max)
+    results = main(text, args.query, args.max)
+
+    for r in results:
+        print(r)
