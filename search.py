d83726177174d41b86ddb0a7750b4b4ac9366857
Sam Lavigne <splavigne@gmail.com>
2021-06-22 18:38:20 -0400

    update

--- search.py
+++ search.py
@@ -1,3 +1,8 @@
+"""
+This is basically just a command line tool that wraps the semantic search example from:
+https://www.sbert.net/examples/applications/semantic-search/README.html
+"""
+
 import sys
 import argparse
 import pickle
@@ -23,14 +28,20 @@ def main(text, query, max_results=100, memoize=True):
         with open(picklename, "rb") as filein:
             corpus_embeddings = pickle.load(filein)
     else:
-        corpus_embeddings = embedder.encode(corpus, convert_to_tensor=True, show_progress_bar=True)
+        corpus_embeddings = embedder.encode(
+            corpus, convert_to_tensor=True, show_progress_bar=True
+        )
         if memoize:
             with open(picklename, "wb") as fileout:
-                pickle.dump(corpus_embeddings, fileout, protocol=pickle.HIGHEST_PROTOCOL)
+                pickle.dump(
+                    corpus_embeddings, fileout, protocol=pickle.HIGHEST_PROTOCOL
+                )
 
     # Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
     top_k = min(max_results, len(corpus))
-    query_embedding = embedder.encode(query, convert_to_tensor=True, show_progress_bar=False)
+    query_embedding = embedder.encode(
+        query, convert_to_tensor=True, show_progress_bar=False
+    )
 
     # We use cosine-similarity and torch.topk to find the highest 5 scores
     cos_scores = util.pytorch_cos_sim(query_embedding, corpus_embeddings)[0]
@@ -40,14 +51,6 @@ def main(text, query, max_results=100, memoize=True):
         # print(corpus[idx], "(Score: {:.4f})".format(score))
         print(corpus[idx])
 
-    """
-    # Alternatively, we can also use util.semantic_search to perform cosine similarty + topk
-    hits = util.semantic_search(query_embedding, corpus_embeddings, top_k=5)
-    hits = hits[0]      #Get the hits for the first query
-    for hit in hits:
-        print(corpus[hit['corpus_id']], "(Score: {:.4f})".format(hit['score']))
-    """
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="Semantic Searcher")
@@ -55,7 +58,7 @@ if __name__ == "__main__":
         "infile", nargs="?", type=argparse.FileType("r"), default=sys.stdin
     )
     parser.add_argument("--query", "-q")
-    parser.add_argument("--max", default=100)
+    parser.add_argument("--max", type=int, default=100)
 
     args = parser.parse_args()
     text = args.infile.read()
